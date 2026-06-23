61169cedfdcc5f77285cffd66b5daf4f56199992
Rohit Bhandari <rohitbhandari.work@gmail.com>
2026-06-23 17:50:11 +0530

    Convert skill bars to proof-card grid matching section style

--- assets/js/main.js
+++ assets/js/main.js
@@ -93,20 +93,6 @@
     });
   }
 
-  const skilsContent = select(".skills-content");
-  if (skilsContent && typeof Waypoint !== "undefined") {
-    new Waypoint({
-      element: skilsContent,
-      offset: "80%",
-      handler: function() {
-        const progress = select(".progress .progress-bar", true);
-        progress.forEach((el) => {
-          el.style.width = el.getAttribute("aria-valuenow") + "%";
-        });
-      }
-    });
-  }
-
   window.addEventListener("load", () => {
     if (typeof AOS !== "undefined") {
       AOS.init({
