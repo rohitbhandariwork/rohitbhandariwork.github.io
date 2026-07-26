3a24f71b468ce20baa14ada98178e20b46d77130
Rohit Bhandari <rohitbhandari.work@gmail.com>
2026-07-26 12:23:08 +0530

    Fix routing (hash strategy), topbar (52px blur), brand.md layout ref

--- ionic-project/src/app/pages/app-18plus/app-18plus.page.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  standalone: false,
-  selector: 'app-age-18plus',
-  templateUrl: './app-18plus.page.html',
-  styleUrls: ['./app-18plus.page.scss'],
-})
-export class App18plusPage {}
